57b696003fa40131889529379552808bba1517db
joselo <joseloc@gmail.com>
2024-12-04 16:39:17 -0500

    add config functions to billing core module

--- lib/billing_core.ex
+++ lib/billing_core.ex
@@ -1,18 +1,43 @@
 defmodule BillingCore do
-  @moduledoc """
-  Documentation for `BillingCore`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  def decimals do
+    2
+  end
 
-  ## Examples
+  def reception_url do
+    "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl"
+  end
 
-      iex> BillingCore.hello()
-      :world
+  def authorization_url do
+    "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"
+  end
 
-  """
-  def hello do
-    :world
+  def prod_reception_url do
+    "https://cel.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl"
+  end
+
+  def prod_authorization_url do
+    "https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"
+  end
+
+  def soap_server_timeout do
+    900_000
+  end
+
+  def soap_server_recv_timeout do
+    900_000
+  end
+
+  def timeout do
+    900_000
+  end
+
+  def open_ssl_legacy do
+    true
+  end
+
+  def timezone do
+    "America/Guayaquil"
   end
 end
--- test/billing_core_test.exs
+++ test/billing_core_test.exs
@@ -1,8 +1,65 @@
 defmodule BillingCoreTest do
   use ExUnit.Case
-  doctest BillingCore
 
-  test "greets the world" do
-    assert BillingCore.hello() == :world
+  describe "decimals/0" do
+    2
+  end
+
+  describe "reception_url/0" do
+    test "returns the test reception url" do
+      assert BillingCore.reception_url() ==
+               "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl"
+    end
+  end
+
+  describe "authorization_url/0" do
+    test "returns the test authorization url" do
+      assert BillingCore.authorization_url() ==
+               "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"
+    end
+  end
+
+  describe "prod_reception_url/0" do
+    test "returns the production reception url" do
+      assert BillingCore.prod_reception_url() ==
+               "https://cel.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl"
+    end
+  end
+
+  describe "prod_authorization_url/0" do
+    test "returns the production authorization url" do
+      assert BillingCore.prod_authorization_url() ==
+               "https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"
+    end
+  end
+
+  describe "soap_server_timeout/0" do
+    test "returns the timeout for the soap server" do
+      assert BillingCore.soap_server_timeout() == 900_000
+    end
+  end
+
+  describe "soap_server_recv_timeout/0" do
+    test "returns the recv timeout for the soap server" do
+      assert BillingCore.soap_server_recv_timeout() == 900_000
+    end
+  end
+
+  describe "timeout/0" do
+    test "returns the timeout for the sri http client" do
+      assert BillingCore.timeout() == 900_000
+    end
+  end
+
+  describe "open_ssl_legacy/0" do
+    test "returns the flag for open ssl legacy" do
+      assert BillingCore.open_ssl_legacy() == true
+    end
+  end
+
+  describe "timezone/0" do
+    test "returns the default timezone" do
+      assert BillingCore.timezone() == "America/Guayaquil"
+    end
   end
 end
